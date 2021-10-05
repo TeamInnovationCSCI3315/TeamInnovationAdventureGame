e5edd41f92f5e4736f35fac1846984ec04a6f0ba
YashBhawsar08 <72819126+YashBhawsar08@users.noreply.github.com>
2021-10-05 12:14:26 -0400

    Update UI.h

--- UI.h
+++ UI.h
@@ -14,3 +14,43 @@ public:
 	void DisplayLocation(string);
 };
 #endif
+
+
+// adding item descriptions here, not sure if this is a the proper location, but I will review with Andrew tonight
+
+#include "GameClass.h"
+#include "UI.h"
+#include <iostream>
+#include <cstdlib>
+#include <string>
+using namespace std;
+
+
+/* void GameClass::Location()
+{
+	
+}*/
+void GameClass::SetLocation(string location)
+{
+	PlayerLocation = location;
+}
+string GameClass::GetLocation()
+{
+	return PlayerLocation;
+}
+void UIClass::DrinkDescr()
+{
+	cout << "Would you like a drink from the bar?" << endl;
+
+}
+void UIClass::pickuplanternDescr()
+{
+	cout << "Do you want to pickup the lantern?" << endl;
+	
+}
+void UIClass::leaveLanternDescr()
+{
+	cout << "Do you want to leave the lantern?" << endl;
+	cout << "Note: The lantern could be a source of light in dark areas" << endl;
+
+}
